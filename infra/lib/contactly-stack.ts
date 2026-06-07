92da3e069643609874ba0e10500b6e306cb0dfc8
joangavelan <joangavelan@gmail.com>
2026-06-07 11:43:19 -0500

    chore: scaffold cdk infra project

---
+++ infra/lib/contactly-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+
+export class ContactlyStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Resources will be added here phase by phase
+  }
+}
